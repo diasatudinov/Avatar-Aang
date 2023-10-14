1149ddf74a1efc76d6b343b0a066ee5cd808322a
Dias <dias.atudinov@gmail.com>
2023-10-14 19:28:42 +0600

    Initial Commit

---
+++ Avatar Aang/Avatar_AangApp.swift
@@ -0,0 +1,17 @@
+//
+//  Avatar_AangApp.swift
+//  Avatar Aang
+//
+//  Created by Atudinov Dias on 14.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Avatar_AangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
